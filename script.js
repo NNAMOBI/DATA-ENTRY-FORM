200ebdf5c912677069743bedd1c2c1aededf1f72
NNAMOBI <nnamosuagwu@gmail.com>
2019-05-27 20:38:21 +0100

    i validated the form

---
+++ script.js
@@ -0,0 +1,37 @@
+function validateSignUp() {
+    var fname = document.getElementById('firstName');
+    var lname = document.getElementById('lastName');
+     var age = document.getElementById('age');
+    var password = document.getElementById('password');
+    var pwdConfirm = document.getElementById('passwordcheck')
+    var email = document.getElementById('email');
+    var tandc = document.getElementById('tandc');
+    if(Number(age.value) === 0 || Number(age.value) < 0) {
+        alert("Age must be greater than zero");
+        return false;
+    } else if(fname.value === "") {
+        alert("First name is required");
+        return false;
+  }    else if(lname.value === "") {
+            alert("Last name is required");
+            return false;
+    } else if(email.value === "" && !email.value.includes("@")) {
+        alert("Invalid email format");
+        return false;
+     } else if( password.value === "" && password.value.length < 6) {
+            alert("Weak password");
+            return false;
+        
+    } else if( password.value !== pwdConfirm.value ) {
+        alert("Wrong password");
+        return false;
+    
+} else if(!tandc.value){
+        alert("Please agree to term and condition");
+        return false;
+    } else {
+        return true;
+    }
+}
+
+ 
